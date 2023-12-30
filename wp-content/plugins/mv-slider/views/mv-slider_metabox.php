21764677450e377f926c6b322323938dfeb034c6
Rayhanuc <rayhanctg4@gmail.com>
2023-12-30 23:34:57 +0600

    23. Metabox API - Nonces and other validations

--- wp-content/plugins/mv-slider/views/mv-slider_metabox.php
+++ wp-content/plugins/mv-slider/views/mv-slider_metabox.php
@@ -7,6 +7,7 @@
 ?>
 
 <table class="form-table mv-slider-metabox">
+    <input type="hidden" name="mv_slider_nonce" value="<?php echo wp_create_nonce( "mv_slider_nonce" ); ?>" >
 	<tr>
 		<th>
             <label for="mv_slider_link_text"><?php echo esc_html("Link Text");?></label>
