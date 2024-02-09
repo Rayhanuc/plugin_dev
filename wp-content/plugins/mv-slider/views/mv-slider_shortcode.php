6a377b9c32ad4a60709b3ebe5d53335df8c32a0d
Rayhanuc <rayhanctg4@gmail.com>
2024-02-10 04:07:07 +0600

    41. Shortcode API - Creating the shortcode's view (part 3)

--- wp-content/plugins/mv-slider/views/mv-slider_shortcode.php
+++ wp-content/plugins/mv-slider/views/mv-slider_shortcode.php
@@ -11,21 +11,32 @@
         );
 
         $my_query = new WP_Query( $args );
+        if ( $my_query->have_posts() ) :
+          while( $my_query->have_posts() ) : $my_query->the_post();
+
+            $button_text = get_post_meta(get_the_ID(), 'mv_slider_link_text', true);
+            $button_url = get_post_meta(get_the_ID(), 'mv_slider_link_url', true);
         ?>
 		<li>
+            <?php the_post_thumbnail('full', array( 'class' => 'img-fluid' )); ?>
 			<div class="mvs-container">
 				<div class="slider-details-container">
 					<div class="wrapper">
 						<div class="slider-title">
-							<h2>Slider Title</h2>
+							<h2><?php esc_html(the_title()); ?></h2>
 						</div>
 						<div class="slider-description">
-							<div class="subtitle">Subtitle</div>
-							<a href="#" class="link">Button Text</a>
+							<div class="subtitle"><?php esc_html(the_content()); ?>></div>
+							<a href="<?php echo esc_attr($button_url); ?>" class="link"><?php esc_html( $button_text ); ?>></a>
 						</div>
 					</div>
 				</div>
 			</div>
 		</li>
+      <?php
+            endwhile;
+            wp_reset_postdata();
+        endif;
+      ?>
 	</ul>
 </div>
